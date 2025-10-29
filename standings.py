e1303a2de22f64d508a93ddc8774648845df4717
Pete Aykroyd <aykroyd@gmail.com>
2025-10-28 22:50:47 -0400

    Load matches and show something

--- standings.py
+++ standings.py
@@ -4,7 +4,11 @@ import json
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json, LetterCase
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, DataTable
+from textual.widgets import Footer, Header, Label, DataTable, Rule
+from textual.containers import Container, HorizontalGroup, VerticalScroll
+from textual.reactive import reactive
+from textual.css.query import NoMatches
+
 
 
 API_KEY = os.environ.get("FOOTBALL_API_KEY")
@@ -36,6 +40,14 @@ class Standing:
     goal_difference: int
 
 
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Match:
+    id: str
+    home_team: Team
+    away_team: Team
+
+
 def get_standings() -> list[Standing]:
     url = BASE_URL + "/competitions/PL/standings"
     headers = { 'X-Auth-Token': API_KEY }
@@ -48,6 +60,18 @@ def get_standings() -> list[Standing]:
     table = [Standing.from_json(json.dumps(row)) for row in standings[0]["table"]]
 
     return table
+
+
+def get_matches(team: Team):
+    url = BASE_URL + f"/teams/{team.id}/matches/"
+
+    headers = { 'X-Auth-Token': API_KEY }
+    params = {"season": "2025", "competitions": "PL"}
+
+    resp = requests.get(url, params=params, headers=headers)
+    resp.raise_for_status()
+    matches = resp.json()["matches"]
+    return [Match.from_json(json.dumps(m)) for m in matches]
 
 
 class StandingsTable(DataTable):
@@ -76,6 +100,40 @@ class StandingsTable(DataTable):
                           str(team.points))
 
 
+class TeamView(VerticalScroll):
+    
+    name = reactive("")
+    matches: list[Match] = reactive([])
+
+    def compose(self) -> ComposeResult:
+        yield Label(self.name, id="name_label")
+        yield Container(id="matches")
+    
+    def watch_name(self, new_name: str) -> None:
+        try:
+            self.query_one("#name_label", Label).update(new_name)
+        except NoMatches:
+            # This can happen before the label has been initialized.
+            pass
+
+    def watch_matches(self, matches: list[Match]) -> None:
+        try:
+            container = self.query_one("#matches", Container) 
+            container.remove_children()
+            container.mount(*[MatchWidget(m) for m in matches])
+        except NoMatches:
+            pass
+
+        
+class MatchWidget(HorizontalGroup):
+    def __init__(self, match: Match):
+        super().__init__()
+        self.match = match
+
+    def compose(self) -> ComposeResult:
+        yield Label(f"{self.match.home_team.name} v {self.match.away_team.name}")
+
+
 class StandingsApp(App):
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
@@ -83,12 +141,21 @@ class StandingsApp(App):
         super().__init__()
         self.standings = get_standings()
 
+    def on_data_table_row_highlighted(self, selection: DataTable.RowSelected):
+        team_view = self.query_one("#team_view", TeamView)
+        index = selection.data_table.get_row_index(selection.row_key)
+        standing = self.standings[index]
+        matches = get_matches(standing.team)
+        team_view.name = standing.team.name
+        team_view.matches = matches
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
         yield StandingsTable()
-
+        yield Rule(line_style="double")
+        yield TeamView(id="team_view")
 
     def on_mount(self) -> None:
         table = self.query_one(DataTable)
