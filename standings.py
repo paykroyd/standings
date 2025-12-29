b977bcc4eed0d4d939a71ed100444fa7743e02cb
Pete Aykroyd <aykroyd@gmail.com>
2025-12-28 23:03:04 -0500

    re-indent code

--- standings.py
+++ standings.py
@@ -21,127 +21,127 @@ BASE_URL = "http://api.football-data.org/v4"
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass(frozen=True)
 class Team:
-    id: str
-    name: str
-    short_name: str
-    tla: str
-    crest: str
+  id: str
+  name: str
+  short_name: str
+  tla: str
+  crest: str
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class Standing:
-    position: int
-    team: Team
-    played_games: int
-    form: str
-    won: int
-    draw: int
-    lost: int
-    points: int
-    goals_for: int
-    goals_against: int
-    goal_difference: int
+  position: int
+  team: Team
+  played_games: int
+  form: str
+  won: int
+  draw: int
+  lost: int
+  points: int
+  goals_for: int
+  goals_against: int
+  goal_difference: int
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class ScoreSnapshot:
-    home: int
-    away: int
+  home: int
+  away: int
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class Score:
-    winner: str
-    full_time: ScoreSnapshot
+  winner: str
+  full_time: ScoreSnapshot
 
-    @property
-    def home(self):
-        return self.full_time.home
+  @property
+  def home(self):
+    return self.full_time.home
 
-    @property
-    def away(self):
-        return self.full_time.away
+  @property
+  def away(self):
+    return self.full_time.away
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class Match:
-    id: str
-    utc_date: str
-    home_team: Team
-    away_team: Team
-    status: str
-    matchday: int
-    score: Score
+  id: str
+  utc_date: str
+  home_team: Team
+  away_team: Team
+  status: str
+  matchday: int
+  score: Score
 
-    @property
-    def date(self) -> date:
-        dt_object = datetime.fromisoformat(self.utc_date.replace('Z', '+00:00'))
-        return dt_object.date()
+  @property
+  def date(self) -> date:
+    dt_object = datetime.fromisoformat(self.utc_date.replace('Z', '+00:00'))
+    return dt_object.date()
 
-    @property
-    def finished(self) -> bool:
-        return self.status == 'FINISHED'
+  @property
+  def finished(self) -> bool:
+    return self.status == 'FINISHED'
 
 
 def get_standings() -> list[Standing]:
-    url = BASE_URL + "/competitions/PL/standings"
-    headers = { 'X-Auth-Token': API_KEY }
-    params = {"season": "2025" }
+  url = BASE_URL + "/competitions/PL/standings"
+  headers = { 'X-Auth-Token': API_KEY }
+  params = {"season": "2025" }
 
-    resp = requests.get(url, params=params, headers=headers)
-    resp.raise_for_status()
+  resp = requests.get(url, params=params, headers=headers)
+  resp.raise_for_status()
 
-    standings = resp.json()["standings"]
-    table = [Standing.from_json(json.dumps(row)) for row in standings[0]["table"]]
+  standings = resp.json()["standings"]
+  table = [Standing.from_json(json.dumps(row)) for row in standings[0]["table"]]
 
-    return table
+  return table
 
 
 @cache
 def get_matches(team: Team):
-    url = BASE_URL + f"/teams/{team.id}/matches/"
+  url = BASE_URL + f"/teams/{team.id}/matches/"
 
-    headers = { 'X-Auth-Token': API_KEY }
-    params = {"season": "2025", "competitions": "PL"}
+  headers = { 'X-Auth-Token': API_KEY }
+  params = {"season": "2025", "competitions": "PL"}
 
-    resp = requests.get(url, params=params, headers=headers)
-    resp.raise_for_status()
-    matches = resp.json()["matches"]
-    return [Match.from_json(json.dumps(m)) for m in matches]
+  resp = requests.get(url, params=params, headers=headers)
+  resp.raise_for_status()
+  matches = resp.json()["matches"]
+  return [Match.from_json(json.dumps(m)) for m in matches]
 
 
 class StandingsTable(DataTable):
-    ROWS = ["Pos", "Club", "GP", "W", "D", "L", "GF", "GA", "GD", "PTS"] 
+  ROWS = ["Pos", "Club", "GP", "W", "D", "L", "GF", "GA", "GD", "PTS"] 
 
-    def __init__(self):
-        super().__init__()
-        self.standings = []
-        self.cursor_type = "row"
-        self.zebra_stripes = True
-        self.add_columns(*StandingsTable.ROWS)
+  def __init__(self):
+    super().__init__()
+    self.standings = []
+    self.cursor_type = "row"
+    self.zebra_stripes = True
+    self.add_columns(*StandingsTable.ROWS)
 
-    def update_data(self, standings: list[Standing]) -> None:
-        self.standings = standings
-        self.clear()
-        for team in self.standings:
-            self.add_row(str(team.position),
-                          team.team.short_name,
-                          str(team.played_games),
-                          str(team.won),
-                          str(team.draw),
-                          str(team.lost),
-                          str(team.goals_for),
-                          str(team.goals_against),
-                          str(team.goal_difference),
-                          str(team.points))
+  def update_data(self, standings: list[Standing]) -> None:
+    self.standings = standings
+    self.clear()
+    for team in self.standings:
+      self.add_row(str(team.position),
+                   team.team.short_name,
+                   str(team.played_games),
+                   str(team.won),
+                   str(team.draw),
+                   str(team.lost),
+                   str(team.goals_for),
+                   str(team.goals_against),
+                   str(team.goal_difference),
+                   str(team.points))
 
-            
+
 class MatchWidget(Widget):
-    DEFAULT_CSS = """
+  DEFAULT_CSS = """
         MatchWidget {
             height: 4;
             height: 1fr;
@@ -156,42 +156,42 @@ class MatchWidget(Widget):
 
         """
 
-    def __init__(self, match: Match):
-        super().__init__()
-        self.match = match
+  def __init__(self, match: Match):
+    super().__init__()
+    self.match = match
 
-    def compose(self) -> ComposeResult:
-        # TODO: flesh out the Match class and then show more details here.
+  def compose(self) -> ComposeResult:
+    # TODO: flesh out the Match class and then show more details here.
 
-        if self.match.finished:
-            self.styles.height = 5
-            yield Container(
-                Label(self.match.date.strftime("%a %b %d"), id="match_date"),
-                Horizontal(
-                    Label(self.match.home_team.name),
-                    Label(" vs "),
-                    Label(self.match.away_team.name)
-                ),
-                Horizontal(
-                    Label(str(self.match.score.home)),
-                    Label(" - "),
-                    Label(str(self.match.score.away)),
-                )
-            )
-        else:
-            self.styles.height = 4
-            yield Container(
-                Label(self.match.date.strftime("%a %b %d"), id="match_date"),
-                Horizontal(
-                    Label(self.match.home_team.name, id="home_label"),
-                    Label(" vs ", id="vs-label"),
-                    Label(self.match.away_team.name, id="away_label")
-                ),
-            )
+    if self.match.finished:
+      self.styles.height = 5
+      yield Container(
+        Label(self.match.date.strftime("%a %b %d"), id="match_date"),
+        Horizontal(
+          Label(self.match.home_team.name),
+          Label(" vs "),
+          Label(self.match.away_team.name)
+        ),
+        Horizontal(
+          Label(str(self.match.score.home)),
+          Label(" - "),
+          Label(str(self.match.score.away)),
+        )
+      )
+    else:
+      self.styles.height = 4
+      yield Container(
+        Label(self.match.date.strftime("%a %b %d"), id="match_date"),
+        Horizontal(
+          Label(self.match.home_team.name, id="home_label"),
+          Label(" vs ", id="vs-label"),
+          Label(self.match.away_team.name, id="away_label")
+        ),
+      )
 
 
 class TeamView(VerticalScroll):
-    DEFAULT_CSS = """
+  DEFAULT_CSS = """
     #matches {
         height: auto;
         layout: vertical;
@@ -203,125 +203,125 @@ class TeamView(VerticalScroll):
     }
     """
 
-    name = reactive("")
-    matches: list[Match] = reactive([])
+  name = reactive("")
+  matches: list[Match] = reactive([])
 
-    def compose(self) -> ComposeResult:
-        yield Label(self.name, id="name_label")
-        yield Container(id="matches")
-    
-    def watch_name(self, new_name: str) -> None:
-        try:
-            self.query_one("#name_label", Label).update(new_name)
-        except NoMatches:
-            # This can happen before the label has been initialized.
-            pass
+  def compose(self) -> ComposeResult:
+    yield Label(self.name, id="name_label")
+    yield Container(id="matches")
 
-    async def watch_matches(self, matches: list[Match]) -> None:
-        try:
-            container = self.query_one("#matches", Container) 
-            container.remove_children()
-            widgets = [MatchWidget(m) for m in matches]
-            await container.mount(*widgets)
-            self.scroll_to_unplayed(widgets)
-        except NoMatches:
-            pass
+  def watch_name(self, new_name: str) -> None:
+    try:
+      self.query_one("#name_label", Label).update(new_name)
+    except NoMatches:
+      # This can happen before the label has been initialized.
+      pass
 
-    def scroll_to_unplayed(self, widgets: list[MatchWidget]) -> None:
-        scroll_target = None
-        for child in widgets:
-            child.refresh(layout=True)
-            if scroll_target is None and not child.match.finished:
-                scroll_target = child
-                
-        if scroll_target:
-            self.call_after_refresh(self.scroll_to_widget, scroll_target, animate=False)
-        else:
-            self.scroll_to(y=0, animate=False)
-                
-        
-    def filter(self, filter_fn, scroll_to_unplayed=False) -> None:
-        try:
-            container = self.query_one("#matches", Container)
-            container.remove_children()
-            widgets = [MatchWidget(m) for m in self.matches if filter_fn(m)]
-            container.mount(*widgets)
+  async def watch_matches(self, matches: list[Match]) -> None:
+    try:
+      container = self.query_one("#matches", Container) 
+      container.remove_children()
+      widgets = [MatchWidget(m) for m in matches]
+      await container.mount(*widgets)
+      self.scroll_to_unplayed(widgets)
+    except NoMatches:
+      pass
 
-            if scroll_to_unplayed:
-                self.scroll_to_unplayed(widgets)
-            else:
-                self.scroll_to(y=0, animate=False)
-        except Nomatches:
-            pass
+  def scroll_to_unplayed(self, widgets: list[MatchWidget]) -> None:
+    scroll_target = None
+    for child in widgets:
+      child.refresh(layout=True)
+      if scroll_target is None and not child.match.finished:
+        scroll_target = child
 
-    def filter_to_played(self) -> None:
-        self.filter(lambda m: m.finished)
+    if scroll_target:
+      self.call_after_refresh(self.scroll_to_widget, scroll_target, animate=False)
+    else:
+      self.scroll_to(y=0, animate=False)
 
-    def filter_to_unplayed(self) -> None:
-        self.filter(lambda m: not m.finished)
 
-    def show_all(self) -> None:
-        self.filter(lambda m: True, scroll_to_unplayed=True)
+  def filter(self, filter_fn, scroll_to_unplayed=False) -> None:
+    try:
+      container = self.query_one("#matches", Container)
+      container.remove_children()
+      widgets = [MatchWidget(m) for m in self.matches if filter_fn(m)]
+      container.mount(*widgets)
 
-        
+      if scroll_to_unplayed:
+        self.scroll_to_unplayed(widgets)
+      else:
+        self.scroll_to(y=0, animate=False)
+    except Nomatches:
+      pass
+
+  def filter_to_played(self) -> None:
+    self.filter(lambda m: m.finished)
+
+  def filter_to_unplayed(self) -> None:
+    self.filter(lambda m: not m.finished)
+
+  def show_all(self) -> None:
+      self.filter(lambda m: True, scroll_to_unplayed=True)
+
+
 
 
 class StandingsApp(App):
 
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),
-                ("u", "filter_to_unplayed", "Show unplayed matches"),
-                ("p", "filter_to_played", "Show played matches"),
-                ("a", "show_all", "Show all matches"),]
+  BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),
+              ("u", "filter_to_unplayed", "Show unplayed matches"),
+              ("p", "filter_to_played", "Show played matches"),
+              ("a", "show_all", "Show all matches"),]
 
-    def __init__(self):
-        super().__init__()
-        self.standings = get_standings()
+  def __init__(self):
+    super().__init__()
+    self.standings = get_standings()
 
-    def on_data_table_row_highlighted(self, selection: DataTable.RowSelected):
-        team_view = self.query_one("#team_view", TeamView)
-        index = selection.data_table.get_row_index(selection.row_key)
-        standing = self.standings[index]
-        matches = get_matches(standing.team)
-        team_view.name = standing.team.name
-        team_view.matches = matches
+  def on_data_table_row_highlighted(self, selection: DataTable.RowSelected):
+    team_view = self.query_one("#team_view", TeamView)
+    index = selection.data_table.get_row_index(selection.row_key)
+    standing = self.standings[index]
+    matches = get_matches(standing.team)
+    team_view.name = standing.team.name
+    team_view.matches = matches
 
-    def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
-        yield Header()
-        yield Footer()
-        yield StandingsTable()
-        yield Rule(line_style="double")
-        yield TeamView(id="team_view")
+  def compose(self) -> ComposeResult:
+    """Create child widgets for the app."""
+    yield Header()
+    yield Footer()
+    yield StandingsTable()
+    yield Rule(line_style="double")
+    yield TeamView(id="team_view")
 
-    def on_mount(self) -> None:
-        table = self.query_one(DataTable)
-        table.update_data(self.standings)
+  def on_mount(self) -> None:
+    table = self.query_one(DataTable)
+    table.update_data(self.standings)
 
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
+  def action_toggle_dark(self) -> None:
+    """An action to toggle dark mode."""
+    self.theme = (
+      "textual-dark" if self.theme == "textual-light" else "textual-light"
+    )
 
-    def action_filter_to_unplayed(self) -> None:
-        """Filters the team view to only unplayed matches."""
-        team_view = self.query_one("#team_view", TeamView)
-        team_view.filter_to_unplayed()
+  def action_filter_to_unplayed(self) -> None:
+    """Filters the team view to only unplayed matches."""
+    team_view = self.query_one("#team_view", TeamView)
+    team_view.filter_to_unplayed()
 
-    def action_filter_to_played(self) -> None:
-        """Filters the team view to only unplayed matches."""
-        team_view = self.query_one("#team_view", TeamView)
-        team_view.filter_to_played()
+  def action_filter_to_played(self) -> None:
+    """Filters the team view to only unplayed matches."""
+    team_view = self.query_one("#team_view", TeamView)
+    team_view.filter_to_played()
 
-    def action_show_all(self) -> None:
-        """Filters the team view to only unplayed matches."""
-        team_view = self.query_one("#team_view", TeamView)
-        team_view.show_all()
+  def action_show_all(self) -> None:
+    """Filters the team view to only unplayed matches."""
+    team_view = self.query_one("#team_view", TeamView)
+    team_view.show_all()
 
-        
-            
-        
+
+
+
 if __name__ == '__main__':
-    app = StandingsApp()
-    app.run()
+  app = StandingsApp()
+  app.run()
 
